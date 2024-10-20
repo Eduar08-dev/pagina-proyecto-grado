d826ab460c22a88e507a8670b386b0ac24e8fb8b
Eduardo <eduarm12@hotmail.com>
2024-10-20 18:37:59 -0500

    correcciones en redireccion al presionar boton de registro

--- inicio-sesion.js
+++ inicio-sesion.js
@@ -7,14 +7,16 @@ document.addEventListener('DOMContentLoaded', function() {
         const nombre = document.getElementById('nombre').value;
         const apellido = document.getElementById('apellido').value;
         const email = document.getElementById('email').value;
+        const usuario = document.getElementById('usuario').value;
         const contraseña = document.getElementById('contraseña').value;
         
-        if (nombre && email && contraseña && apellido) {
+        if (nombre && email && contraseña && apellido && usuario) {
 
             console.log('Registro exitoso:');
             console.log('Nombre:', nombre);
             console.log('Apellido',  apellido);
             console.log('Email:', email);
+            console.log('Usuario' , usuario)
             console.log('Contraseña:', contraseña);
             
             form.reset();
